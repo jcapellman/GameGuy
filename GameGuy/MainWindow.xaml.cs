2bd011c904201a11ee7a798766fd45170822c558
Jarred Capellman <jcapellman@hotmail.com>
2019-03-30 11:31:16 -0500

    Added: Initial work on the MainWindow class

---
+++ GameGuy/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+using GameGUY.ViewModels;
+
+namespace GameGUY
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = new MainViewModel();
+        }
+
+        private void ButtonExit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
